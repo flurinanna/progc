4af9e3c0de01d48fb18419e47a17b6587d91d8cf
Thomas Stettler <stetttho@students.zhaw.ch>
2017-10-04 15:18:07 +0200

    fehlende libs fuer tests ergaenzt

---
+++ final_01_celsius_fahrenheit/src/main.c
@@ -0,0 +1,59 @@
+/* ----------------------------------------------------------------------------
+ * --  _____       ______  _____                                              -
+ * -- |_   _|     |  ____|/ ____|                                             -
+ * --   | |  _ __ | |__  | (___    Institute of Embedded Systems              -
+ * --   | | | '_ \|  __|  \___ \   Zuercher Hochschule Winterthur             -
+ * --  _| |_| | | | |____ ____) |  (University of Applied Sciences)           -
+ * -- |_____|_| |_|______|_____/   8401 Winterthur, Switzerland               -
+ * ----------------------------------------------------------------------------
+ */
+/**
+ * 
+ * @brief Calculation of Temperature Fahrenheit to Celsius 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+double calculate (int fheit);
+
+/**
+ * @brief Prints out a table of values between -100 and 200 degrees Fahrenheit. 
+ *        If an int Argument is given, it converts this int value from 
+ *        Fahrenheit to Celsius and prints it out (in addition to the table).
+ * @param[in] argv  argv[1] Temperature in Fahrenheit (optional)
+ */
+int main(int argc, char* argv[])
+{	
+    // printing out the table from -100 to 200 degrees Fahrenheit
+    (void)printf("F'heit    Celsius\n");
+	(void)printf("-----------------\n");
+	int fheit = -100;
+	double celsius;
+	while (fheit <= 200) {
+	    celsius = calculate(fheit);
+	    (void)printf("  %4d    %7.2f\n", fheit, celsius);
+	    fheit += 20;
+	}
+	
+	// if there was an argument passed into the program, a custom calculation is
+	// made here with that argument and printed out.
+	if (argc == 2) {
+	    int fheit_custom = atoi(argv[1]);  // atoi converts the argument to int
+	    double celsius_custom = calculate (fheit_custom);
+	    (void)printf("-----------------\n");
+	    (void)printf("Custom calculation: \n");
+	    (void)printf("  %4d    %7.2f\n", fheit_custom, celsius_custom);        
+	} 
+	    
+	return EXIT_SUCCESS;
+}
+
+/**
+* calculates the celsius value for a fahrenheit value.
+* @param int Fahrenheit
+*/
+double calculate (const int fheit) {
+    double celsius = 0.0;
+    celsius = (5.0*(fheit-32))/9.0;  
+    return celsius;
+}
---
+++ final_01_celsius_fahrenheit/tests/tests.c
@@ -0,0 +1,103 @@
+/* ----------------------------------------------------------------------------
+ * --  _____       ______  _____                                              -
+ * -- |_   _|     |  ____|/ ____|                                             -
+ * --   | |  _ __ | |__  | (___    Institute of Embedded Systems              -
+ * --   | | | '_ \|  __|  \___ \   Zuercher Hochschule Winterthur             -
+ * --  _| |_| | | | |____ ____) |  (University of Applied Sciences)           -
+ * -- |_____|_| |_|______|_____/   8401 Winterthur, Switzerland               -
+ * ----------------------------------------------------------------------------
+ */
+/**
+ * @file
+ * @brief Test suite for the given package.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "CUnit/Basic.h"
+#include "test_utils.h"
+
+
+#ifndef TARGET // must be given by the make file --> see test target
+#error missing TARGET define
+#endif
+
+/// @brief The name of the STDOUT text file.
+#define OUTFILE "stdout.txt"
+/// @brief The name of the STDERR text file.
+#define ERRFILE "stderr.txt"
+
+#define EPSILON 0.001
+// unit under test
+double calculate (int fheit);
+
+// setup & cleanup
+static int setup(void)
+{
+	remove_file_if_exists(OUTFILE);
+	remove_file_if_exists(ERRFILE);
+	return 0; // success
+}
+
+static int teardown(void)
+{
+	// Do nothing.
+	// Especially: do not remove result files - they are removed in int setup(void) *before* running a test.
+	return 0; // success
+}
+
+
+// tests
+static void test_main_ausgabe(void)
+{
+	// arrange
+	const char *out_txt[] = { 
+	        "F'heit    Celsius\n",
+	        "-----------------\n",
+	        "  -100     -73.33\n",
+	        "   -80     -62.22\n",
+	        "   -60     -51.11\n",
+	        "   -40     -40.00\n",
+	        "   -20     -28.89\n",
+	        "     0     -17.78\n",
+	        "    20      -6.67\n",
+	        "    40       4.44\n",
+	        "    60      15.56\n",
+	        "    80      26.67\n",
+	        "   100      37.78\n",
+	        "   120      48.89\n",
+	        "   140      60.00\n",
+	        "   160      71.11\n",
+	        "   180      82.22\n",
+	        "   200      93.33\n"
+
+	    };
+	        
+	// act
+	int exit_code = system(XSTR(TARGET) " 1>" OUTFILE);
+	// assert
+	CU_ASSERT_EQUAL(exit_code, 0);
+	assert_lines(OUTFILE, out_txt, sizeof(out_txt)/sizeof(*out_txt));
+}
+
+static void test_umrechnung(void)
+{
+	// arrange
+	// act
+	double d = calculate(60);
+	printf("%f", d);
+	// assert
+	CU_ASSERT_DOUBLE_EQUAL(d, 15.5555, EPSILON);
+}
+
+
+/**
+ * @brief Registers and runs the tests.
+ */
+int main(void)
+{
+	// setup, run, teardown
+	TestMainBasic("Celsius Fahrenheit", setup, teardown
+				  , test_main_ausgabe
+			      , test_umrechnung
+				  );
+}
