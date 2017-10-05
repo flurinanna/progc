281cced6d131c765d67ad08c5ebea2705167633d
anna kalin <annaflurina@gmail.com>
2017-10-05 10:34:22 +0200

    tests für aufgabe 02 hinzugefügt

--- 02_selbststudium_anna/tests/tests.c
+++ 02_selbststudium_anna/tests/tests.c
@@ -25,6 +25,17 @@
 /// @brief The name of the STDERR text file.
 #define ERRFILE "stderr.txt"
 
+
+typedef struct {
+int day;
+int month;
+int year;
+} Date;
+
+/// UUT - Unit-Under-Test
+Date berechne_folgetag(Date start_date);
+int teste_gueltigkeit(Date start_date);
+
 // setup & cleanup
 static int setup(void)
 {
@@ -42,43 +53,103 @@ static int teardown(void)
 
 
 // tests
-static void test_main_with_zero_args(void)
+static void test_berechnung_schaltjahr(void)
 {
 	// arrange
-	const char *out_txt[] = { "Program name: " XSTR(TARGET) "\n", "Hello World!\n" };
-	const char *err_txt[] = { };
+	Date start_date = {28, 02, 2004};
+	Date next_date = {29, 02, 2004};
 	// act
-	int exit_code = system(XSTR(TARGET) " 1>" OUTFILE " 2>" ERRFILE);
+	Date result_date = berechne_folgetag(start_date);
 	// assert
-	CU_ASSERT_EQUAL(exit_code, 0);
-	assert_lines(OUTFILE, out_txt, sizeof(out_txt)/sizeof(*out_txt));
-	assert_lines(ERRFILE, err_txt, sizeof(err_txt)/sizeof(*err_txt));
+	CU_ASSERT_EQUAL(next_date.day, result_date.day);
+	CU_ASSERT_EQUAL(next_date.month, result_date.month);
+	CU_ASSERT_EQUAL(next_date.year, result_date.year);
 }
 
-static void test_main_with_one_arg(void)
+// tests
+static void test_berechnung_monatwechsel(void)
 {
 	// arrange
-	const char *out_txt[] = { "Program name: " XSTR(TARGET) "\n", "Hello everybody\n" };
-	const char *err_txt[] = { };
+	Date start_date = {30, 4, 2016};
+	Date next_date = {1, 5, 2016};
 	// act
-	int exit_code = system(XSTR(TARGET) " everybody 1>" OUTFILE " 2>" ERRFILE);
+	Date result_date = berechne_folgetag(start_date);
 	// assert
-	CU_ASSERT_EQUAL(exit_code, 0);
-	assert_lines(OUTFILE, out_txt, sizeof(out_txt)/sizeof(*out_txt));
-	assert_lines(ERRFILE, err_txt, sizeof(err_txt)/sizeof(*err_txt));
+	CU_ASSERT_EQUAL(next_date.day, result_date.day);
+	CU_ASSERT_EQUAL(next_date.month, result_date.month);
+	CU_ASSERT_EQUAL(next_date.year, result_date.year);
 }
 
-static void test_main_with_two_args(void)
+// tests
+static void test_berechnung_jahreswechsel(void)
 {
 	// arrange
-	const char *out_txt[] = { "Program name: " XSTR(TARGET) "\n" };
-	const char *err_txt[] = { };
+	Date start_date = {31, 12, 2004};
+	Date next_date = {1, 1, 2005};
 	// act
-	int exit_code = system(XSTR(TARGET) " A B 1>" OUTFILE " 2>" ERRFILE);
+	Date result_date = berechne_folgetag(start_date);
 	// assert
-	CU_ASSERT_EQUAL(exit_code, (1 << 8));
-	assert_lines(OUTFILE, out_txt, sizeof(out_txt)/sizeof(*out_txt));
-	assert_lines(ERRFILE, err_txt, sizeof(err_txt)/sizeof(*err_txt));
+	CU_ASSERT_EQUAL(next_date.day, result_date.day);
+	CU_ASSERT_EQUAL(next_date.month, result_date.month);
+	CU_ASSERT_EQUAL(next_date.year, result_date.year);
+}
+
+// tests
+static void test_berechnung_erstes_datum(void)
+{
+	// arrange
+	Date start_date = {1, 1, 1583};
+	Date next_date = {2, 1, 1583};
+	// act
+	Date result_date = berechne_folgetag(start_date);
+	// assert
+	CU_ASSERT_EQUAL(next_date.day, result_date.day);
+	CU_ASSERT_EQUAL(next_date.month, result_date.month);
+	CU_ASSERT_EQUAL(next_date.year, result_date.year);
+}
+
+// tests
+static void test_teste_gueltigkeit_vor_erstem_datum(void)
+{
+	// arrange
+	Date start_date = {31, 12, 1582};
+	// act
+	int gueltig = teste_gueltigkeit(start_date);
+	// assert
+	CU_ASSERT_EQUAL(1, gueltig);
+}
+
+// tests
+static void test_teste_gueltigkeit_von_datum_tag(void)
+{
+	// arrange
+	Date start_date = {32, 12, 2014};
+	// act
+	int gueltig = teste_gueltigkeit(start_date);
+	// assert
+	CU_ASSERT_EQUAL(1, gueltig);
+}
+
+// tests
+static void test_teste_gueltigkeit_von_datum_monat(void)
+{
+	// arrange
+	Date start_date = {31, 13, 2014};
+	// act
+	int gueltig = teste_gueltigkeit(start_date);
+	// assert
+	CU_ASSERT_EQUAL(1, gueltig);
+}
+
+// tests
+static void test_teste_gueltigkeit_von_datum_in_zukunft(void)
+{
+	// arrange
+	Date start_date = {15, 11, 2022};
+	// act
+	int gueltig = teste_gueltigkeit(start_date);
+	// assert
+	CU_ASSERT_EQUAL(0, gueltig);
 }
 
 /**
@@ -87,9 +158,14 @@ static void test_main_with_two_args(void)
 int main(void)
 {
 	// setup, run, teardown
-	TestMainBasic("Hello World", setup, teardown
-				 // , test_main_with_zero_args
-				 // , test_main_with_one_arg
-				//  , test_main_with_two_args
+	TestMainBasic("Berechung Datum", setup, teardown
+				  , test_berechnung_schaltjahr
+				  , test_berechnung_monatwechsel
+				  , test_berechnung_jahreswechsel
+				  , test_berechnung_erstes_datum
+				  , test_teste_gueltigkeit_vor_erstem_datum
+				  , test_teste_gueltigkeit_von_datum_tag
+				  , test_teste_gueltigkeit_von_datum_monat
+				  , test_teste_gueltigkeit_von_datum_in_zukunft
 				  );
 }
