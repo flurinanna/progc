3435399dabef7571f94fbece556857a846e8ecde
stt <stetttho@students.zhaw.ch>
2017-10-13 19:56:25 +0200

    doku 2 finalisiert

--- final_02_berechne_folgetag/src/main.c
+++ final_02_berechne_folgetag/src/main.c
@@ -13,7 +13,6 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 enum {Jan = 1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
 
@@ -23,7 +22,6 @@ typedef struct {
     int year;
 } Date;
 
-
 Date berechne_folgetag(Date start_date);
 int teste_gueltigkeit(Date start_date);
 
@@ -31,7 +29,7 @@ int teste_gueltigkeit(Date start_date);
  * @brief Read an input-date and prints out the date of the day
  * after the input-date.
  * @returns Returns EXIT_SUCCESS (=0) on success,
- *                  EXIT_FAILURE (=1) if more than one argument is given.
+ *                  EXIT_FAILURE (=1) if given Date is invalid.
  */
 int main(void)
 {
@@ -44,22 +42,20 @@ int main(void)
     start_date.month = m;
     start_date.year = y;
 
-//function call to test the input value 
-int testwert = teste_gueltigkeit(start_date);
+    //function call to test the input value 
+    int testwert = teste_gueltigkeit(start_date);
 
-
-//if the given date is ok > function call to calculate the date of the next day and print out the result
+    //if the given date is ok > function call to calculate the date of the 
+    //next day and print out the result
     if (testwert == 0) {   
     Date next_date = berechne_folgetag(start_date);     
-    (void)printf("Der Folgetag ist %d %d %d\n", next_date.day, next_date.month, next_date.year);
+    (void)printf("Der Folgetag ist der %d.%d.%d\n", 
+                 next_date.day, next_date.month, next_date.year);
 	    return EXIT_SUCCESS;
 	} else { 
 	    return EXIT_FAILURE;
 	}
-    return EXIT_SUCCESS;
 }
-
-
 
 /**
  * @brief calculates the date one day after the input-date
@@ -67,12 +63,12 @@ int testwert = teste_gueltigkeit(start_date);
  */
 Date berechne_folgetag(Date start_date) 
 {
-//case: change of year
+    //case: change of year
     if (start_date.month == Dec && start_date.day == 31) {
         start_date.year++;
         start_date.month = 1;
         start_date.day = 1;
-//case: change of month
+    //case: change of month
     } else if ((start_date.month == Jan || 
                 start_date.month == Mar ||
                 start_date.month == May ||
@@ -87,7 +83,7 @@ Date berechne_folgetag(Date start_date)
                 start_date.month == Nov) && start_date.day == 30) {
         start_date.month++;
         start_date.day = 1;
-//case: month february
+    //case: month february
     } else if(start_date.month == Feb && start_date.day == 28) {
            if ((start_date.year%4 == 0 && 
                 start_date.year%100 != 0)|| 
@@ -97,13 +93,12 @@ Date berechne_folgetag(Date start_date)
                 start_date.month++;
                 start_date.day = 1;
            }
+    //case: change of day
     } else {
-//case: change of day
         start_date.day++;
     }
     return start_date;  
 }
-
 
 /**
  * @brief test, if the given input is a correct date
@@ -111,22 +106,19 @@ Date berechne_folgetag(Date start_date)
  */
 int teste_gueltigkeit(const Date start_date) 
 {
-//test, if it is in the range
+    //test, if it is in the range
     if (start_date.year >= 1583 && start_date.day >= 1 && 
             start_date.day <= 31 && start_date.month >= 1 &&
-            start_date.month <= 12) {
+            start_date.month <= Dec) {
         return EXIT_SUCCESS;
-//test, if its out of the range
+    //test, if its out of the range
     } else if (start_date.day < 1 || start_date.day > 31 ||
-            start_date.month < 1 || start_date.month > 12 ||
+            start_date.month < Jan || start_date.month > Dec ||
             start_date.year < 1583) {
         return EXIT_FAILURE;
-//any other cases
+    //any other cases
     } else {
         return EXIT_FAILURE;
     }
 }
-
-
-
 
